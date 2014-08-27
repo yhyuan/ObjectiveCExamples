ee7003aced8612954e42812a82f84350e24f41df
Jerry Yuan <yhyuan@gmail.com>
2014-08-27 11:22:38 -0400

    Basic syntax for function

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -1,14 +1,16 @@
 //make LDFLAGS="-lbsd"
 #import <Foundation/Foundation.h>
 
-int getRandomInteger(int minimum, int maximum) {
-	return 2; //arc4random_uniform((maximum - minimum) + 1) + minimum;
+NSString *getRandomMake(NSArray *makes) {
+	int maximum = (int)[makes count];
+	int randomIndex = 1; //arc4random_uniform(maximum);
+	return makes[randomIndex];
 }
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-		int randomNumber = getRandomInteger(-10, 10);
-		NSLog(@"Selected a random number between -10 and 10: %d", randomNumber);
+		NSArray *makes = @[@"Honda", @"Ford", @"Nissan", @"Porsche"];
+		NSLog(@"Selected a %@", getRandomMake(makes));
 	}
 	return 0;
 } 
