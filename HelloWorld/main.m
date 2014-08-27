68a41b40bc215f8a9cbbc3397e4fe03691772a19
Jerry Yuan <yhyuan@gmail.com>
2014-08-27 10:51:11 -0400

    Use pointer in array

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -2,12 +2,13 @@
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-		int year = 1967;
-		int *pointer;
-		pointer = &year;
-		NSLog(@"%d", *pointer);
-		*pointer = 1990;
-		NSLog(@"%d", year);
+		char model[5] = {'H','o','n','d','a'};
+		char *modelPointer = &model[0];
+		for (int i=0; i<5; i++) {
+			NSLog(@"Value at memeory address %p is %c", modelPointer, *modelPointer);
+			modelPointer++;
+		}
+		NSLog(@"The first letter is %c", *(modelPointer - 5));
 	}
 	return 0;
 } 
