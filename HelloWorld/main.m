300e105358f0857aac5ad6b09cbdab2d1e41ec4f
Jerry Yuan <yhyuan@gmail.com>
2014-08-27 10:53:37 -0400

    Null Pointer

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -2,13 +2,10 @@
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-		char model[5] = {'H','o','n','d','a'};
-		char *modelPointer = &model[0];
-		for (int i=0; i<5; i++) {
-			NSLog(@"Value at memeory address %p is %c", modelPointer, *modelPointer);
-			modelPointer++;
-		}
-		NSLog(@"The first letter is %c", *(modelPointer - 5));
+		int year = 1967;
+		int *pointer = &year;
+		NSLog(@"%d", *pointer);
+		pointer = NULL;
 	}
 	return 0;
 } 
