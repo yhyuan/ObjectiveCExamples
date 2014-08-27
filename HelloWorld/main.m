3214207f59ae999f95fea57072906999ee311233
Jerry Yuan <yhyuan@gmail.com>
2014-08-27 10:46:33 -0400

    pointer

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -2,11 +2,12 @@
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-		int years[4] = {1968, 1970, 1989, 1999};
-		years[0] = 1967;
-		for (int i=0; i<4; i++) {
-			NSLog(@"The year at index %d is %d", i, years[i]);
-		}
+		int year = 1967;
+		int *pointer;
+		pointer = &year;
+		NSLog(@"%d", *pointer);
+		*pointer = 1990;
+		NSLog(@"%d", year);
 	}
 	return 0;
 } 
