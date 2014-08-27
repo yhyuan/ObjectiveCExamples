c4288bc644b46a08427826b39934f2e22be06fec
Jerry Yuan <yhyuan@gmail.com>
2014-08-27 10:30:30 -0400

    Macro

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -1,12 +1,12 @@
 #import <Foundation/Foundation.h>
 
+#define PI 3.14159
+#define RAD_TO_DEG(radians) (radians * (180 / PI)
+
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-		double odometer = 9200.8;
-		int odometerAsInteger = (int)odometer;
-
-		NSLog(@"You've driven %.1f miles", odometer);
-		NSLog(@"You've driven %d miles", odometerAsInteger);
+		double angle = PI / 2;
+		NSLog(@"%f", RAD_TO_DEG(angle));
 	}
 	return 0;
 }
